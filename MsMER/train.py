ad19353c7187cdab51c7a53f6d0d147950971cc1
Yang Zhao Zhao <65065301+freedompuls@users.noreply.github.com>
2023-10-09 16:15:58 +0800

    Delete MsMER directory

--- MsMER/train.py
+++
@@ -1,6 +0,0 @@
-from pytorch_lightning.utilities.cli import LightningCLI
-
-from msmer.datamodule import CROHMEDatamodule
-from msmer.lit_msmer import LitMsMER
-
-cli = LightningCLI(LitMsMER, CROHMEDatamodule)
